e6bac7fab08eaa36a8dbde44018eeb903b132750
walid <walid@DESKTOP-ML1GK5D>
2021-04-02 01:40:01 +0200

    Add project files.

---
+++ SqlBackup/BackupService.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlBackup
+{
+    public class BackupService
+    {
+        private readonly string _connectionString;
+        private readonly string _backupFolderFullPath;
+        private readonly string[] _systemDatabaseNames = { "master", "tempdb", "model", "msdb" };
+
+        public BackupService(string connectionString, string backupFolderFullPath)
+        {
+            _connectionString = connectionString;
+            _backupFolderFullPath = backupFolderFullPath;
+        }
+
+        public void BackupAllUserDatabases()
+        {
+            Parallel.ForEach(GetAllUserDatabases(), databaseName =>
+            {
+                BackupDatabase(databaseName, Properties.AppSettings.Default.DeleteOlderBackups);
+            });
+        }
+
+        public void BackupDatabase(string databaseName, bool compress = false)
+        {
+            string filePath = BuildBackupPathWithFilename(databaseName);
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var query = String.Format(Properties.AppSettings.Default.SQLBackupCommand, databaseName, filePath);
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            if (compress)
+            {
+                CompressBackup(filePath);
+            }
+        }
+
+        private void CompressBackup(string filePath)
+        {
+            Console.WriteLine(filePath);
+        }
+
+        private IEnumerable<string> GetAllUserDatabases()
+        {
+            var databases = new List<String>();
+
+            DataTable databasesTable;
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                databasesTable = connection.GetSchema("Databases");
+
+                connection.Close();
+            }
+
+            foreach (DataRow row in databasesTable.Rows)
+            {
+                string databaseName = row["database_name"].ToString();
+
+                if (_systemDatabaseNames.Contains(databaseName))
+                    continue;
+
+                databases.Add(databaseName);
+            }
+
+            return databases;
+        }
+
+        private string BuildBackupPathWithFilename(string databaseName)
+        {
+            string filename = string.Format(Properties.AppSettings.Default.BackupFileFormmat + ".bak", databaseName, DateTime.Now.ToString(Properties.AppSettings.Default.BackupFileDateFormmat));
+
+            string fullPath = Path.Combine(_backupFolderFullPath, databaseName);
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(fullPath);
+
+            if (!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+            }
+            else
+            {
+                // delete older backups for saving disk space
+                if (Properties.AppSettings.Default.DeleteOlderBackups)
+                {
+                    Parallel.ForEach(directoryInfo.GetFiles()
+                                .Where(fileInfo => DateTime.Now > fileInfo.CreationTime.AddDays(7)),
+                                fileInfo =>
+                                {
+                                    fileInfo.Delete();
+                                });
+                }
+            }
+
+            return Path.Combine(fullPath, filename);
+        }
+
+
+    }
+}
---
+++ SqlBackup/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlBackup
+{
+    class Program
+    {
+        IDictionary<string, object> keyValuePairs = new Dictionary<string, object>();
+
+        static void Main(string[] args)
+        {
+            string BackupFolderFullPath = Properties.AppSettings.Default.BackupFolderFullPath;
+            string backupFolderFullPath =
+              string.IsNullOrEmpty(BackupFolderFullPath)
+                ? AppDomain.CurrentDomain.BaseDirectory
+                : BackupFolderFullPath;
+
+            string connectionString = "data source=.;initial catalog=IR-NE-Prod;integrated security=True;MultipleActiveResultSets=True;";
+
+            BackupService backupService = new BackupService(connectionString, backupFolderFullPath);
+            backupService.BackupAllUserDatabases();
+
+        }
+    }
+}
