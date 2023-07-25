9f912a92e9e7edcb5d17adcbd3e7f27e1bc92b1e
Walid Khalifa <wkhalifa@shory.com>
2023-07-25 18:38:09 +0300

    20230725183805

--- SqlBackup/Logger.cs
+++ SqlBackup/Logger.cs
@@ -3,36 +3,36 @@ using System.IO;
 
 namespace SqlBackup
 {
-    public static class Logger
-    {
-        public static void LogError(string message, params object[] args)
-        {
-            var formattedMessage = string.Format(message, args);
-            Console.WriteLine($"[{DateTime.Now:T} ERR]: {formattedMessage}");
+	public static class Logger
+	{
+		public static void LogError(string message, params object[] args)
+		{
+			var formattedMessage = string.Format(message, args);
+			Console.WriteLine($"[{DateTime.Now:T} ERR]: {formattedMessage}");
 
-            var logLine = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}:[ERROR] {formattedMessage}";
-            Log(logLine);
-        }
+			var logLine = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}:[ERROR] {formattedMessage}";
+			Log(logLine);
+		}
 
-        public static void LogInformation(string message, params object[] args)
-        {
-            var formattedMessage = string.Format(message, args);
-            Console.WriteLine($"[{DateTime.Now:T} INF]: {formattedMessage}");
+		public static void LogInformation(string message, params object[] args)
+		{
+			var formattedMessage = string.Format(message, args);
+			Console.WriteLine($"[{DateTime.Now:T} INF]: {formattedMessage}");
 
-            var logLine = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}:[INFO]  {formattedMessage}";
-            Log(logLine);
-        }
+			var logLine = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}:[INFO]  {formattedMessage}";
+			Log(logLine);
+		}
 
 
-        private static void Log(string logLine)
-        {
-            // Write the log line to a file
-            var logFilePath =
-                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"); // Replace with your desired file path
-            using (var writer = File.AppendText(logFilePath))
-            {
-                writer.WriteLine(logLine);
-            }
-        }
-    }
+		private static void Log(string logLine)
+		{
+			// Write the log line to a file
+			var logFilePath =
+				Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"); // Replace with your desired file path
+			using (var writer = File.AppendText(logFilePath))
+			{
+				writer.WriteLine(logLine);
+			}
+		}
+	}
 }
--- SqlBackup/Program.cs
+++ SqlBackup/Program.cs
@@ -6,115 +6,111 @@ using System.IO;
 
 namespace SqlBackup
 {
-    internal class Program
-    {
-        private static readonly DateTime DateTime = DateTime.Now;
+	internal class Program
+	{
+		private static readonly DateTime DateTime = DateTime.Now;
 
-        private static readonly string FolderPath = Path.Combine(
-            AppSettings.Default.BackupFolder,
-            $"SQLBackups{DateTime.Year}",
-            DateTime.ToString("MM"),
-            DateTime.ToString("dd"));
-
-        private static readonly string ConnectionString = AppSettings.Default.ConnectionString;
+		private static readonly string FolderPath = AppSettings.Default.BackupFolder;
+		private static readonly string ConnectionString = AppSettings.Default.ConnectionString;
 
 
-        private static void Main(string[] args)
-        {
-            try
-            {
-                using (var connection = new SqlConnection(ConnectionString))
-                {
-                    connection.Open();
+		//private static readonly string FolderPath = Path.Combine(
+		//	AppSettings.Default.BackupFolder,
+		//	$"SQLBackups{DateTime.Year}",
+		//	DateTime.ToString("MM"),
+		//	DateTime.ToString("dd"));
 
-                    // Get the list of databases on the server
-                    var query = @"
-                            SELECT db.name
-                            FROM sys.databases AS db
-                            WHERE 1 = 1
-                                  AND db.name NOT IN ( 'master', 'model', 'msdb', 'tempdb' )
-                                  AND db.state = 0
-                                  AND db.is_in_standby = 0";
+		private static void Main(string[] args)
+		{
+			try
+			{
+				using (var connection = new SqlConnection(ConnectionString))
+				{
+					connection.Open();
 
-                    query += !string.IsNullOrEmpty(AppSettings.Default.SqlCommand)
-                        ? $" AND db.{AppSettings.Default.SqlCommand};"
-                        : ";";
+					// Get the list of databases on the server
+					var query =
+						@"SELECT db.name FROM sys.databases AS db WHERE 1 = 1 AND db.name NOT IN ( 'master', 'model', 'msdb', 'tempdb' ) AND db.state = 0 AND db.is_in_standby = 0";
 
-                    using (var command = new SqlCommand(query, connection))
-                    {
-                        using (var reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                var databaseName = reader.GetString(0);
-                                BackupDatabase(databaseName, FolderPath);
-                            }
-                        }
-                    }
-                }
+					query += !string.IsNullOrEmpty(AppSettings.Default.SqlCommand)
+						? $" AND db.{AppSettings.Default.SqlCommand};"
+						: ";";
 
-                if (AppSettings.Default.CompressBackup) Archive();
+					using (var command = new SqlCommand(query, connection))
+					{
+						using (var reader = command.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								var databaseName = reader.GetString(0);
+								BackupDatabase(databaseName, FolderPath);
+							}
+						}
+					}
+				}
 
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError("An error occurred while backing up databases: " + ex.Message);
-            }
+				if (AppSettings.Default.CompressBackup) Archive();
+			}
+			catch (Exception ex)
+			{
+				Logger.LogError("An error occurred while backing up databases: " + ex.Message);
+			}
 
-            Logger.LogInformation("Press any key to exit...");
-        }
+			Logger.LogInformation("Press any key to exit...");
+		}
 
-        private static void Archive()
-        {
-            foreach (var file in Directory.GetFiles(FolderPath, "*.bak", SearchOption.TopDirectoryOnly))
-            {
-                var targetName = $"{Path.GetDirectoryName(file)}\\{Path.GetFileNameWithoutExtension(file)}.7z";
-                using (var process = new Process())
-                {
-                    var info = new ProcessStartInfo
-                    {
-                        FileName = $"{AppDomain.CurrentDomain.BaseDirectory}\\7z.exe",
+		private static void Archive()
+		{
+			foreach (var file in Directory.GetFiles(FolderPath, "*.bak", SearchOption.TopDirectoryOnly))
+			{
+				var targetName = $"{Path.GetDirectoryName(file)}\\{Path.GetFileNameWithoutExtension(file)}.7z";
+				using (var process = new Process())
+				{
+					var info = new ProcessStartInfo
+					{
+						FileName = $"{AppDomain.CurrentDomain.BaseDirectory}\\7z.exe",
 
-                        /*
-                         Here is the breakdown of each option used:
-                            a: Short for "add", this option specifies that we want to create a new archive.
-                            -t7z: Specifies the type of archive we want to create, in this case a 7z archive.
-                            -mx9: Specifies the maximum compression level, which in this case is 9 (ultra).
-                            -aoa: Specifies that we want to overwrite any existing archive without prompting.
-                        */
-                        Arguments = $"a -t7z -mx9 -aoa \"{targetName}\" \"{file}\"",
-                        WindowStyle = ProcessWindowStyle.Hidden
-                    };
+						/*
+						 Here is the breakdown of each option used:
+						    a: Short for "add", this option specifies that we want to create a new archive.
+						    -t7z: Specifies the type of archive we want to create, in this case a 7z archive.
+						    -mx9: Specifies the maximum compression level, which in this case is 9 (ultra).
+						    -aoa: Specifies that we want to overwrite any existing archive without prompting.
+						*/
+						Arguments = $"a -t7z -mx9 -aoa \"{targetName}\" \"{file}\"",
+						WindowStyle = ProcessWindowStyle.Hidden
+					};
 
-                    process.StartInfo = info;
-                    process.Start();
-                    process.WaitForExit();
-                }
+					process.StartInfo = info;
+					process.Start();
+					process.WaitForExit();
+				}
 
-                File.Delete(file);
+				File.Delete(file);
 
-                Logger.LogInformation($"Archive of file '{Path.GetFileNameWithoutExtension(file)}' completed successfully.");
-            }
-        }
+				Logger.LogInformation(
+					$"Archive of file '{Path.GetFileNameWithoutExtension(file)}' completed successfully.");
+			}
+		}
 
 
-        private static void BackupDatabase(string databaseName, string folderPath)
-        {
-            if (!Directory.Exists(folderPath))
-                Directory.CreateDirectory(folderPath);
+		private static void BackupDatabase(string databaseName, string folderPath)
+		{
+			if (!Directory.Exists(folderPath))
+				Directory.CreateDirectory(folderPath);
 
-            var backupFileName = $"{folderPath}\\{databaseName}_{DateTime:yyyyMMddHHmmss}.bak";
+			var backupFileName = $"{folderPath}\\{databaseName}_{DateTime:yyyyMMddHHmmss}.bak";
 
-            // Perform the backup
-            var query = $"BACKUP DATABASE [{databaseName}] TO DISK='{backupFileName}' WITH INIT";
-            using (var connection = new SqlConnection(ConnectionString))
-            {
-                connection.Open();
-                using (var command = new SqlCommand(query, connection))
-                    command.ExecuteNonQuery();
-            }
+			// Perform the backup
+			var query = $"BACKUP DATABASE [{databaseName}] TO DISK='{backupFileName}' WITH INIT";
+			using (var connection = new SqlConnection(ConnectionString))
+			{
+				connection.Open();
+				using (var command = new SqlCommand(query, connection))
+					command.ExecuteNonQuery();
+			}
 
-            Logger.LogInformation($"Backup of database '{databaseName}' completed successfully.");
-        }
-    }
+			Logger.LogInformation($"Backup of database '{databaseName}' completed successfully.");
+		}
+	}
 }
